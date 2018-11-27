69f36b9b4a3ae741a85234ef8d9d3f4c88e65202
Henrique <henrique.a.gelio@hotmail.com>
2018-11-27 07:49:34 +0000

    Include user mailer with account activation and some tests. Pending to update create action

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,10 +26,17 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # Don't care if the mailer can't send (set false).
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
+  
+  config.action_mailer.delivery_method = :test
+  host = 'cff23579e61c44028cc97742b8f8ebb8.vfs.cloud9.us-east-2.amazonaws.com/' # Don't use this literally; use your local dev host instead
+  # Use this on the cloud IDE.
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+  # Use this if developing on localhost.
+  # config.action_mailer.default_url_options = { host: host, protocol: 'http' }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
